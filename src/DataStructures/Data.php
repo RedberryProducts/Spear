ad1982e6e31e26f8d67b0a9cb21866068292ea6e
kenke11 <kenkebashvili2@gmail.com>
2022-05-19 14:23:58 +0400

    docs: adding comments for data structures

--- src/DataStructures/Data.php
+++ src/DataStructures/Data.php
@@ -6,12 +6,24 @@ use Redberry\Spear\Interfaces\Data as ContractsData;
 
 class Data implements ContractsData
 {
+	/**
+	 * Creating props for result code.
+	 */
 	private int $resultCode;
 
+	/**
+	 * Creating props for error.
+	 */
 	private string|null $error;
 
+	/**
+	 * Creating props for output code.
+	 */
 	private string|null $output;
 
+	/**
+	 * Grants to the props default meanings.
+	 */
 	public function __construct()
 	{
 		$this->resultCode = 0;
@@ -19,36 +31,57 @@ class Data implements ContractsData
 		$this->output = null;
 	}
 
+	/**
+	 * Set result code for code execution.
+	 */
 	public function setResultCode(int $code): void
 	{
 		$this->resultCode = $code;
 	}
 
+	/**
+	 * Set error message when something went wrong.
+	 */
 	public function setErrorMessage(string $error): void
 	{
 		$this->error = trim($error);
 	}
 
+	/**
+	 * Set output returns executed code.
+	 */
 	public function setOutput(string $output): void
 	{
 		$this->output = trim($output);
 	}
 
+	/**
+	 * Get resulted code.
+	 */
 	public function getResultCode(): int
 	{
 		return $this->resultCode;
 	}
 
+	/**
+	 * Get error massage.
+	 */
 	public function getErrorMessage(): string|null
 	{
 		return $this->error;
 	}
 
+	/**
+	 * Get output result.
+	 */
 	public function getOutput(): string|null
 	{
 		return $this->output;
 	}
 
+	/**
+	 * Returns array with all details.
+	 */
 	public function toArray(): array
 	{
 		return [
