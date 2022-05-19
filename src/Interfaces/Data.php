e9f69badcbbff3357a0121ef1d66c1104f56464a
kenke11 <kenkebashvili2@gmail.com>
2022-05-19 14:31:38 +0400

    docs: adding comments for interfaces

--- src/Interfaces/Data.php
+++ src/Interfaces/Data.php
@@ -4,17 +4,38 @@ namespace Redberry\Spear\Interfaces;
 
 interface Data
 {
+	/**
+	 * setResultCode must be used in class.
+	 */
 	public function setResultCode(int $code): void;
 
+	/**
+	 * setErrorMessage must be used in class.
+	 */
 	public function setErrorMessage(string $error): void;
 
+	/**
+	 * setOutput must be used in class.
+	 */
 	public function setOutput(string $output): void;
 
+	/**
+	 * getResultCode must be used in class.
+	 */
 	public function getResultCode(): int;
 
+	/**
+	 * getErrorMessage must be used in class.
+	 */
 	public function getErrorMessage(): string|null;
 
+	/**
+	 * getOutput must be used in class.
+	 */
 	public function getOutput(): string|null;
 
+	/**
+	 * toArray must be used in class.
+	 */
 	public function toArray(): array;
 }
--- src/Interfaces/Handler.php
+++ src/Interfaces/Handler.php
@@ -4,5 +4,8 @@ namespace Redberry\Spear\Interfaces;
 
 interface Handler
 {
+	/**
+	 * This invoke function must be used in class.
+	 */
 	public function __invoke(string $code, string $input = ''): Data;
 }
